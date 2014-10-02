74694a1e2b614d7ecffd009b57f75d545acb5fbc
Fabio Maggio <fabio@infoweb.be>
2014-10-02 14:49:14 +0200

    Add eventhandler for pjax grid

--- assets/js/cms.js
+++ assets/js/cms.js
@@ -16,13 +16,32 @@
     CMS.init = function() {
         // Set global eventhandlers
         $(document)
-            .on('afterValidate', '.tabbed-form', CMS.showFirstFormTabWithErrors);    
+            .on('afterValidate', '.tabbed-form', CMS.showFirstFormTabWithErrors)
+            .on('click', '#grid-pjax [data-toggle-active]', CMS.pjaxGridItemToggleActive);    
     };
     
     // Shows the first tab on a tabbed form that contains validation errors
     CMS.showFirstFormTabWithErrors = function(e) {
         if ($(".has-error").length)
             $("a[href=#"+$(".has-error").parents(".tab-pane").attr("id")+"]").tab("show");    
+    };
+    
+    // Toggles the 'active' state of an item in a PJAX grid
+    CMS.pjaxGridItemToggleActive = function(e) {
+        e.preventDefault();
+        
+        var id = $(this).data('toggle-active'),
+            request = $.post('active', {id:id});
+        
+        request.done(function(response) {
+            // Succes, reload PJAX grid
+            if (response == 1) {
+                $.pjax.reload({container:'#grid-pjax'});
+            } else {
+                // @todo: catch error
+                return false;                
+            }
+        });
     };
     
     return CMS;    

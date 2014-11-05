ea7e2a1d10c149ed64b85426708dacbaf074d07b
Ruben Heymans <ruben.heymans@gmail.com>
2014-11-05 10:30:55 +0100

    Add editor options

--- Module.php
+++ Module.php
@@ -8,20 +8,20 @@ use \yii\helpers\Url;
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'infoweb\cms\controllers';
-    
+
     /**
      * @var array   The items that are shown in the sidebar navigation
      */
     public $sideBarItems = [];
-    
+
     public function init()
     {
         parent::init();
     }
-    
+
     /**
      * Returns the items for the sidebar, formatted for usage in the SideNav widget
-     * 
+     *
      * @param   string  $group      The group that contains the items
      * @param   string  $template   The template that is used to render the html of the items
      * @return  array
@@ -29,19 +29,62 @@ class Module extends \yii\base\Module
     public function getSideBarItems($group = '', $template = '<a href="{url}">{icon}<span class="nav-label">{label}</span></a>')
     {
         $items = [];
-        
+
         if (!isset($this->sideBarItems[$group]))
             return $items;
-        
+
         foreach ($this->sideBarItems[$group] as $item) {
             $items[] = [
                 'label'     => Yii::t($item['i18nGroup'], $item['label']),
                 'url'       => Url::toRoute($item['url']),
                 'template'  => $template,
                 'visible'   => (isset($item['authItem']) && Yii::$app->user->can($item['authItem'])) ? true : false
-            ];    
+            ];
         }
-        
-        return $items;    
+
+        return $items;
+    }
+
+    public function getCKEditorStylesheets() {
+
+        // Get the bootstrap asset url
+        $bootstrapAssetUrl = Yii::$app->assetManager->getAssetUrl(\yii\bootstrap\BootstrapAsset::register(Yii::$app->view), 'css/bootstrap.css');
+
+        // Get the font asset
+        $fontAsset = new \frontend\assets\FontAsset;
+
+        // Add default css
+        $css = [$bootstrapAssetUrl, Yii::getAlias('@frontendUrl') . '/css/main.css', Yii::getAlias('@frontendUrl') . '/css/editor.css'];
+
+        // Add google fonts
+        foreach ($fontAsset->css as $font) {
+            $css[] = $fontAsset->basePath . '/' . $font;
+        }
+
+        return $css;
+    }
+
+    public function getCKEditorOptions() {
+
+        $editorOptions = [
+            'height' => 300,
+            'preset' => 'custom',
+            'toolbarGroups' => [
+                ['name' => 'clipboard', 'groups' => ['mode','undo', 'selection', 'clipboard', 'doctools']],
+                ['name' => 'editing', 'groups' => ['tools']],
+                ['name' => 'paragraph', 'groups' => ['templates', 'list', 'indent', 'align']],
+                ['name' => 'insert'],
+                ['name' => 'basicstyles', 'groups' => ['basicstyles', 'cleanup']],
+                ['name' => 'colors'],
+                ['name' => 'links'],
+                ['name' => 'others'],
+            ],
+            'removeButtons' => 'Smiley,Iframe,Templates,Outdent,Indent,Flash,Table,SpecialChar,PageBreak',
+            'contentsCss' => $this->getCKEditorStylesheets(),
+            'extraAllowedContent' => 'div(*)',
+            'extraPlugins' => 'codemirror',
+        ];
+
+        return $editorOptions;
     }
 }

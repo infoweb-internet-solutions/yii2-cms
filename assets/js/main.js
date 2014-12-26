48f6be5555839e79a77e6fcb8bdbda2adbe476db
Ruben Heymans <ruben.heymans@gmail.com>
2014-12-26 11:55:09 +0100

    Update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,56 @@
+
 /*
-yii.allowAction = function ($e) {
-    var message = $e.data('confirm');
-    return message === undefined || yii.confirm(message, $e);
-};
-yii.confirm = function (message, $e) {
-    bootbox.confirm(message, function (confirmed) {
-        if (confirmed) {
-            yii.handleAction($e);
+// --- Delete action (bootbox) ---
+yii.confirm = function (message, ok, cancel) {
+
+    bootbox.confirm({
+        message: message,
+        buttons: {
+            confirm: {
+                label: "OK"
+            },
+            cancel: {
+                label: "Cancel"
+            }
+        },
+        callback: function (confirmed) {
+
+            if (confirmed) {
+                //!ok || ok();
+
+                // @todo Delete action ajax request
+                /*
+                var category = $(this).data('category'),
+                    id = $(this).data('id'),
+                    request = $.post('delete', {id:id});
+
+                request.done(function(response) {
+                    // Succes, reload PJAX grid
+                    if (response == 1) {
+                        $.pjax.reload({container: '#grid-pjax' + ((category) ? '-' + category : '')});
+                    } else {
+                        // @todo: catch error
+                        return false;
+                    }
+                });
+
+
+            } else {
+                !cancel || cancel();
+            }
+
+            if (confirmed) {
+                //$.pjax.reload({container: '#grid-pjax'});
+            }
         }
     });
+
     // confirm will always return false on the first call
     // to cancel click handler
     return false;
 }
- */
+*/
+
 $(function() {
 
     /*

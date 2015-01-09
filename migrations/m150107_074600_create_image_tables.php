d3b5673263f6a370d3f8da1968b8261f444b2efc
Fabio Maggio <fabio@infoweb.be>
2015-01-09 14:04:20 +0100

    Fix error in migration

--- migrations/m150107_074600_create_image_tables.php
+++ migrations/m150107_074600_create_image_tables.php
@@ -19,7 +19,7 @@ class m150107_074600_create_image_tables extends Migration
         }
 
         $this->createTable('image', [
-            'id' => 'pk',
+            'id' => Schema::TYPE_PK,
             'filePath' => 'VARCHAR(400) NOT NULL',
             'itemId' => 'int(20) NOT NULL',
             'isMain' => 'int(1)',
@@ -28,13 +28,13 @@ class m150107_074600_create_image_tables extends Migration
             'position' => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
             'created_at' => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
             'updated_at' => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
-        ]);
+        ], $tableOptions);
 
         $this->createIndex('itemId', '{{%image}}', 'itemId');
         
         // Create 'image_lang' table
         $this->createTable('{{%image_lang}}', [
-            'image_id'              => Schema::TYPE_INTEGER . '(10) UNSIGNED NOT NULL',
+            'image_id'              => Schema::TYPE_INTEGER . 'NOT NULL',
             'language'              => Schema::TYPE_STRING . '(2) NOT NULL',
             'alt'                   => Schema::TYPE_STRING . ' NOT NULL',
             'title'                 => Schema::TYPE_STRING . ' NOT NULL',
@@ -45,7 +45,7 @@ class m150107_074600_create_image_tables extends Migration
 
         $this->addPrimaryKey('image_lang_image_id_language', '{{%image_lang}}', ['image_id', 'language']);
         $this->createIndex('language', '{{%image_lang}}', 'language');
-        $this->addForeignKey('FK_IMAGE_LANG_IMAGE_ID', '{{%image_lang}}', 'image_id', '{{%image}}', 'id', 'CASCADE', 'NO ACTION');
+        $this->addForeignKey('FK_IMAGE_LANG_IMAGE_ID', '{{%image_lang}}', 'image_id', '{{%image}}', 'id', 'CASCADE', 'RESTRICT');
     }
 
     public function down()

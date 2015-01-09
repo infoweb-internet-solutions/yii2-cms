b7238f77642999dab18b2a9854888984cacdd2f7
Fabio Maggio <fabio@infoweb.be>
2015-01-09 13:17:00 +0100

    Update migration

--- migrations/m150107_074600_create_image.php
+++ migrations/m150107_074600_create_image.php
@@ -7,6 +7,12 @@ class m150107_074600_image extends Migration
 {
     public function up()
     {
+        $tableOptions = null;
+
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+        
         // Drop image table
         if ($this->db->schema->getTableSchema('image', true)) {
             $this->dropTable('image');
@@ -25,10 +31,26 @@ class m150107_074600_image extends Migration
         ]);
 
         $this->createIndex('itemId', '{{%image}}', 'itemId');
+        
+        // Create 'image_lang' table
+        $this->createTable('{{%image_lang}}', [
+            'image_id'              => Schema::TYPE_INTEGER . '(10) UNSIGNED NOT NULL',
+            'language'              => Schema::TYPE_STRING . '(2) NOT NULL',
+            'alt'                   => Schema::TYPE_STRING . ' NOT NULL',
+            'title'                 => Schema::TYPE_STRING . ' NOT NULL',
+            'description'           => Schema::TYPE_TEXT . ' NOT NULL',
+            'created_at'            => Schema::TYPE_INTEGER . '(10) UNSIGNED NOT NULL',
+            'updated_at'            => Schema::TYPE_INTEGER . '(10) UNSIGNED NOT NULL',
+        ], $tableOptions);
+
+        $this->addPrimaryKey('image_lang_image_id_language', '{{%image_lang}}', ['image_id', 'language']);
+        $this->createIndex('language', '{{%image_lang}}', 'language');
+        $this->addForeignKey('FK_IMAGE_LANG_IMAGE_ID', '{{%image_lang}}', 'image_id', '{{%image}}', 'id', 'CASCADE', 'NO ACTION');
     }
 
     public function down()
     {
+        $this->dropTable('image_lang');
         $this->dropTable('image');
     }
 }

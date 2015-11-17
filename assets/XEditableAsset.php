9e44aa1ccb135742306bee886a1913437f5a6bfb
Fabio Maggio <fabiomaggio@gmail.com>
2015-11-17 08:56:44 +0100

    Use bower version of the asset

--- assets/XEditableAsset.php
+++ assets/XEditableAsset.php
@@ -5,19 +5,13 @@ use yii\web\AssetBundle as AssetBundle;
 
 class XEditableAsset extends AssetBundle
 {
-    public $sourcePath = '@infoweb/cms/assets/vendor/bootstrap3-editable';
-    
+    public $sourcePath = '@bower/bootstrap3-editable';
+
     public $js = [
         'js/bootstrap-editable.js'
     ];
-    
+
     public $css = [
-        'css/bootstrap-editable.css'    
-    ];
-    
-    public $depends = [
-        'backend\assets\AppAsset',
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapPluginAsset',
+        'css/bootstrap-editable.css'
     ];
 }

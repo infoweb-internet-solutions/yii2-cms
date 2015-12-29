9e4009d697dfc7116976b89724e04fc3b633110f
Ruben Heymans <ruben.heymans@gmail.com>
2015-12-29 15:27:57 +0100

    Update migrations

--- migrations/m150107_074600_create_image_tables.php
+++ migrations/m150107_074600_create_image_tables.php
@@ -19,31 +19,32 @@ class m150107_074600_create_image_tables extends Migration
         }
 
         $this->createTable('{{%image}}', [
-            'id'                    => Schema::TYPE_PK,
-            'filePath'              => Schema::TYPE_STRING . '(400) NOT NULL',
-            'itemId'                => Schema::TYPE_STRING . '(255) NOT NULL',
-            'isMain'                => 'TINYINT(3) UNSIGNED NOT NULL DEFAULT \'1\'',
-            'modelName'             => Schema::TYPE_STRING . '(255) NOT NULL',
-            'urlAlias'              => Schema::TYPE_STRING . '(255) NOT NULL',
-            'position'              => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
-            'active'                => 'TINYINT(3) UNSIGNED NOT NULL DEFAULT \'1\'',
-            'created_at'            => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
-            'updated_at'            => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
+            'id'         => Schema::TYPE_PK,
+            'name'       => Schema::TYPE_STRING . '(255) NOT NULL',
+            'filePath'   => Schema::TYPE_STRING . '(400) NOT NULL',
+            'itemId'     => Schema::TYPE_STRING . '(255) NOT NULL',
+            'isMain'     => 'TINYINT(3) UNSIGNED NOT NULL DEFAULT \'1\'',
+            'modelName'  => Schema::TYPE_STRING . '(255) NOT NULL',
+            'urlAlias'   => Schema::TYPE_STRING . '(255) NOT NULL',
+            'position'   => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
+            'active'     => 'TINYINT(3) UNSIGNED NOT NULL DEFAULT \'1\'',
+            'created_at' => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
+            'updated_at' => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
         ], $tableOptions);
 
         $this->createIndex('itemId', '{{%image}}', 'itemId');
 
         // Create 'image_lang' table
         $this->createTable('{{%image_lang}}', [
-            'image_id'              => Schema::TYPE_INTEGER . ' NOT NULL',
-            'language'              => Schema::TYPE_STRING . '(2) NOT NULL',
-            'alt'                   => Schema::TYPE_STRING . '(255) NOT NULL',
-            'title'                 => Schema::TYPE_STRING . '(255) NOT NULL',
-            'subtitle'              => Schema::TYPE_STRING . ' NOT NULL',
-            'description'           => Schema::TYPE_TEXT . ' NOT NULL',
-            'url'                   => Schema::TYPE_STRING . '(255) NOT NULL',
-            'created_at'            => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
-            'updated_at'            => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
+            'image_id'    => Schema::TYPE_INTEGER . ' NOT NULL',
+            'language'    => Schema::TYPE_STRING . '(2) NOT NULL',
+            'alt'         => Schema::TYPE_STRING . '(255) NOT NULL',
+            'title'       => Schema::TYPE_STRING . '(255) NOT NULL',
+            'subtitle'    => Schema::TYPE_STRING . ' NOT NULL',
+            'description' => Schema::TYPE_TEXT . ' NOT NULL',
+            'url'         => Schema::TYPE_STRING . '(255) NOT NULL',
+            'created_at'  => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
+            'updated_at'  => Schema::TYPE_INTEGER . ' UNSIGNED NOT NULL',
         ], $tableOptions);
 
         $this->addPrimaryKey('image_lang_image_id_language', '{{%image_lang}}', ['image_id', 'language']);

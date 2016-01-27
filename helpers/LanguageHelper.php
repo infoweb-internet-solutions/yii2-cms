f4022b784e6ba5212cd115dabf2628c48ac48d28
Fabio Maggio <fabiomaggio@gmail.com>
2016-01-27 16:46:23 +0100

    Add helpers for the different parts of the language code

--- helpers/LanguageHelper.php
+++ helpers/LanguageHelper.php
@@ -25,4 +25,39 @@ class LanguageHelper
 
         return false;
     }
+
+    /**
+     * Returns the different parts of the provided or current language code.
+     *     "en-US" => ['en', 'us']
+     *
+     * @return array
+     */
+    public static function getLanguageCodeParts($language = null)
+    {
+        return array_map('strtolower', explode('-', ($language) ? $language : Yii::$app->language));
+    }
+
+    /**
+     * Returns only the language part of the provided or current language code.
+     *     "en-US" => 'en'
+     *
+     * @return string
+     */
+    public static function getLanguage($language = null)
+    {
+        return self::getLanguageCodeParts($language)[0];
+    }
+
+    /**
+     * Returns only the country part of the language code.
+     *     "en-US" => 'us'
+     *
+     * @return string
+     */
+    public static function getCountry($language = null)
+    {
+        $parts = self::getLanguageCodeParts($language);
+
+        return (isset($parts[1])) ? $parts[1] : '';
+    }
 }

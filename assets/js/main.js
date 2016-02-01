f54dee858c1e032be3483931552687e4a248c28e
Ruben Heymans <ruben.heymans@gmail.com>
2016-02-01 14:05:50 +0100

    Update delete modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,71 +1,23 @@
-
-/*
-// --- Delete action (bootbox) ---
+/**
+ * Overwrite default delete confirm dialog
+ *
+ * @param message
+ * @param ok
+ * @param cancel
+ * @returns {boolean}
+ */
 yii.confirm = function (message, ok, cancel) {
-
-    bootbox.confirm({
-        message: message,
-        buttons: {
-            confirm: {
-                label: "OK"
-            },
-            cancel: {
-                label: "Cancel"
-            }
-        },
-        callback: function (confirmed) {
-
-            if (confirmed) {
-                //!ok || ok();
-
-                // @todo Delete action ajax request
-                /*
-                var category = $(this).data('category'),
-                    id = $(this).data('id'),
-                    request = $.post('delete', {id:id});
-
-                request.done(function(response) {
-                    // Succes, reload PJAX grid
-                    if (response == 1) {
-                        $.pjax.reload({container: '#grid-pjax' + ((category) ? '-' + category : '')});
-                    } else {
-                        // @todo: catch error
-                        return false;
-                    }
-                });
-
-
-            } else {
-                !cancel || cancel();
-            }
-
-            if (confirmed) {
-                //$.pjax.reload({container: '#grid-pjax'});
-            }
+    bootbox.confirm(message, function (confirmed) {
+        if (confirmed) {
+            !ok || ok();
+        } else {
+            !cancel || cancel();
         }
     });
-
-    // confirm will always return false on the first call
-    // to cancel click handler
     return false;
-}
-*/
+};
 
 $(function() {
-
-    /*
-    // Minimalize menu when screen is less than 768px
-    $(function() {
-        $(window).bind("load resize", function() {
-            if ($(this).width() < 769) {
-                $('body').addClass('body-small')
-            } else {
-                $('body').removeClass('body-small')
-            }
-        })
-    });
-    */
-
     // Init CMS module
     CMS.init();
 });

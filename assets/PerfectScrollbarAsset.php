9bead76f0a7ed943bf57d73719ce16f7c690b2f1
Kurt Aerts <kurt@infoweb.be>
2016-06-24 14:36:18 +0200

    Fix perfect scrollbar asset bundle

--- assets/PerfectScrollbarAsset.php
+++ assets/PerfectScrollbarAsset.php
@@ -12,6 +12,6 @@ class PerfectScrollbarAsset extends AssetBundle
     ];
 
     public $js = [
-        'js/perfect-scrollbar.jquery.min.js'
+        'js/min/perfect-scrollbar.jquery.min.js'
     ];
 }
